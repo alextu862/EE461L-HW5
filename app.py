fe51ef465ad0178ec0d2e4ccb624da575f22adea
alextu862 <alextu863@utexas.edu>
2025-12-02 03:47:55 -0600

    Add projectId to join and leave project responses

--- app.py
+++ app.py
@@ -1,25 +1,34 @@
-# app.py
 from flask import Flask, jsonify
 from flask_cors import CORS
 
 app = Flask(__name__)
-CORS(app)  # so React (localhost or Heroku) can call these routes
+CORS(app)
+
+
+@app.route("/")
+def home():
+    return "HW5 backend is running"
+
 
 @app.route("/checkin/<projectId>/<qty>")
 def checkIn_hardware(projectId, qty):
     return jsonify({"message": f"{qty} hardware checked in", "projectId": projectId})
 
+
 @app.route("/checkout/<projectId>/<qty>")
 def checkOut_hardware(projectId, qty):
     return jsonify({"message": f"{qty} hardware checked out", "projectId": projectId})
 
+
 @app.route("/join/<projectId>")
 def joinProject(projectId):
-    return jsonify({"message": f"Joined {projectId}"})
+    return jsonify({"message": f"Joined {projectId}", "projectId": projectId})
+
 
 @app.route("/leave/<projectId>")
 def leaveProject(projectId):
-    return jsonify({"message": f"Left {projectId}"})
+    return jsonify({"message": f"Left {projectId}", "projectId": projectId})
+
 
 if __name__ == "__main__":
     app.run()
